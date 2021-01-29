f9d7f3184c52bc562e73ca3054064f5534c3f81a
Carmelo CANTA <carmelo.canta@hotmail.com>
2021-01-29 20:30:50 +0100

    Update back port 3000 into 4242

--- back/src/main.ts
+++ back/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from "./app.module";
 
 async function bootstrap () {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  console.log("** Toppics back server running at localhost:3000 **");
+  await app.listen(4242);
+  console.log("** Toppics back server running at localhost:4242 **");
 }
 bootstrap();
