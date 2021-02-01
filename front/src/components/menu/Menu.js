04614ad1df072cc5f034571e81295d6ed5467bc8
unknown <dyrloz@gmail.com>
2021-02-01 09:12:32 +0100

    (FRONT) Modification on Menu

--- front/src/components/menu/Menu.js
+++ front/src/components/menu/Menu.js
@@ -3,7 +3,7 @@ import React from 'react';
 import home from '../../images/home.svg';
 import search from '../../images/search.svg';
 import play from '../../images/play.svg';
-import post from '../../images/post.svg';
+import publication from '../../images/post.svg';
 import { NavLink } from 'react-router-dom';
 
 function Menu () {
@@ -12,7 +12,7 @@ function Menu () {
                 <NavLink exact to='/' className='menu-item' activeClassName='selected'><img className='menu-img' src={home}/></NavLink>
                 <NavLink to='/search' className='menu-item' activeClassName='selected'><img className='menu-img' src={search}/></NavLink>
                 <NavLink to='/play' className='menu-item' activeClassName='selected'><img className='menu-img' src={play}/></NavLink>
-                <NavLink to='/post' className='menu-item' activeClassName='selected'><img className='menu-img' src={post}/></NavLink>
+                <NavLink to='/publication' className='menu-item' activeClassName='selected'><img className='menu-img' src={publication}/></NavLink>
             </div>
     );    
 }
